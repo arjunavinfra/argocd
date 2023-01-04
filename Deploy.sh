c523cb50a53931221af7146b183005a879fcbe74
Arjun <arjun@infracloud.io>
2023-01-04 12:25:39 +0530

    corrected the issue

---
+++ Deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+which argocd 
+if [ $? != 0 ]; then
+
+wget https://github.com/argoproj/argo-cd/releases/download/v2.5.5/argocd-linux-amd64 -O /tmp/argocd
+chmod +x /tmp/argocd 
+sudo mv /tmp/argocd  /sbin/argocd 
+else 
+echo "argocd Cli is aliready installed"
+fi 
+
+kubectl apply -k ./Install
+
+echo -e "\n"
+echo -e "Admin Password:  `kubectl -n argocd  get secret argocd-initial-admin-secret -ojson   | jq .data.password -r | base64 -d`"
+echo -e "\n"
